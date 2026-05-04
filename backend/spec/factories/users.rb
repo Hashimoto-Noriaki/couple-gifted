cf3db035bcff4a94ca0d2214d9bc05d2f9b5565c
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-05-04 18:22:48 +0900

    Factory作成

---
+++ backend/spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.unique.email }
+    password { 'password123' }
+    password_confirmation { 'password123' }
+    nickname { Faker::Name.name }
+    gender { 'male' }
+    relationship_status { 'dating' }
+  end
+end
