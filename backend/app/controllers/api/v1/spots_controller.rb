047d42b11b3ee811857d8e1aa1118cad055e491c
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-05-10 01:00:16 +0900

    フィルタリングロジックをモデルスコープに移動

--- backend/app/controllers/api/v1/spots_controller.rb
+++ backend/app/controllers/api/v1/spots_controller.rb
@@ -2,9 +2,7 @@ module Api
   module V1
     class SpotsController < ApplicationController
       def index
-        spots = Spot.all
-        spots = spots.joins(:tags).where(tags: { name: params[:tag] }).distinct if params[:tag].present?
-        spots = spots.where("address LIKE ?", "%#{params[:area]}%") if params[:area].present?
+        spots = Spot.all.by_tag(params[:tag]).by_area(params[:area])
         render json: SpotSerializer.new(spots).serializable_hash
       end
 
