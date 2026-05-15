65ece0c32d392a0965679b9fc2541108462ff50b
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-05-16 03:37:14 +0900

    テスト環境のHost Authorizationを修正

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -72,6 +72,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:each, type: :request) do
+    host! "localhost"
+  end
 end
 
 Shoulda::Matchers.configure do |config|
