cdeb1dd9bed851dc0e0cb9bcf826aaab284cc3d9
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-08-04 00:02:59 +0900

    Factory.をつけなくてもテストを書けるように設定

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -69,4 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Allow calling FactoryBot methods (create, build, ...) without the `FactoryBot.` prefix.
+  config.include FactoryBot::Syntax::Methods
 end
